f145a7fd7a9c3456bc2cac2fdd6d4d56803810a3
JustUzair <uzairhajra763390@gmail.com>
2023-09-11 21:39:54 +0530

    Temporary fix for motion div

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-import herobg from "./assets/herobg.png"
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import herobg from "./assets/herobg.png";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -15,24 +25,29 @@ const App = () => {
         <Tech />
         <Works />
         {/* <Feedbacks /> */}
-        
-        <div className='relative z-0' style={{
-          background:`url(${herobg})`
-        }}>
-          <div style={{
-          width:"100%",
-          height:"20vh",
-          position:"absolute",
-          background:"linear-gradient(to top, transparent, rgba(5,8,22,1))",
-          // zIndex:1000
-        }}></div>
+
+        <div
+          className="relative z-0"
+          style={{
+            background: `url(${herobg})`,
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "20vh",
+              position: "absolute",
+              background:
+                "linear-gradient(to top, transparent, rgba(5,8,22,1))",
+              // zIndex:1000
+            }}
+          ></div>
           <Contact />
-          
         </div>
         <StarsCanvas />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
